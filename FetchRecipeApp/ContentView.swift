9d731559d4b80085db193af376e6d63a0750fb6c
Pragna Elavarthi <pragnaelavarthi04@gmail.com>
2025-03-12 23:55:42 -0700

    Initial Commit

---
+++ FetchRecipeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FetchRecipeApp
+//
+//  Created by Pragna Elavarthi on 3/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppApp.swift
+//  FetchRecipeApp
+//
+//  Created by Pragna Elavarthi on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
